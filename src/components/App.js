c7a6307b914ba349ca5b90b90ebff04ce068ec8d
rexxx18 <65245341+rexxx18@users.noreply.github.com>
2020-10-24 12:02:02 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [paragraph, setparagraph] = useState(false);
+  const handleChange = () => {
+    setparagraph(true);
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleChange}>
+        click
+      </button>
+      {paragraph ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : null}
     </div>
   );
 }
-
 
 export default App;
